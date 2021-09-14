e0c3859722cc281dfb311d3317b03c5538c8676d
Ngo Pham Tu Uyen <1910155@dlu.edu.vn>
2021-09-14 15:39:38 +0700

    first commit

---
+++ Lab03_Demo/Lab03_Demo/Form2.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace Lab03_Demo
+{
+    partial class frmTuyChon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.rdMaSV = new System.Windows.Forms.RadioButton();
+            this.rdHoTen = new System.Windows.Forms.RadioButton();
+            this.rdNgaySinh = new System.Windows.Forms.RadioButton();
+            this.txtTimKiem = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            this.btnSapXep = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.Color.LightPink;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(34, 43);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(450, 70);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // rdMaSV
+            // 
+            this.rdMaSV.AutoSize = true;
+            this.rdMaSV.BackColor = System.Drawing.Color.LightPink;
+            this.rdMaSV.Location = new System.Drawing.Point(50, 70);
+            this.rdMaSV.Name = "rdMaSV";
+            this.rdMaSV.Size = new System.Drawing.Size(70, 21);
+            this.rdMaSV.TabIndex = 1;
+            this.rdMaSV.TabStop = true;
+            this.rdMaSV.Text = "Mã SV";
+            this.rdMaSV.UseVisualStyleBackColor = false;
+            // 
+            // rdHoTen
+            // 
+            this.rdHoTen.AutoSize = true;
+            this.rdHoTen.BackColor = System.Drawing.Color.LightPink;
+            this.rdHoTen.Location = new System.Drawing.Point(197, 70);
+            this.rdHoTen.Name = "rdHoTen";
+            this.rdHoTen.Size = new System.Drawing.Size(71, 21);
+            this.rdHoTen.TabIndex = 2;
+            this.rdHoTen.TabStop = true;
+            this.rdHoTen.Text = "Họ tên";
+            this.rdHoTen.UseVisualStyleBackColor = false;
+            // 
+            // rdNgaySinh
+            // 
+            this.rdNgaySinh.AutoSize = true;
+            this.rdNgaySinh.BackColor = System.Drawing.Color.LightPink;
+            this.rdNgaySinh.Location = new System.Drawing.Point(335, 70);
+            this.rdNgaySinh.Name = "rdNgaySinh";
+            this.rdNgaySinh.Size = new System.Drawing.Size(92, 21);
+            this.rdNgaySinh.TabIndex = 3;
+            this.rdNgaySinh.TabStop = true;
+            this.rdNgaySinh.Text = "Ngày sinh";
+            this.rdNgaySinh.UseVisualStyleBackColor = false;
+            // 
+            // txtTimKiem
+            // 
+            this.txtTimKiem.BackColor = System.Drawing.Color.White;
+            this.txtTimKiem.Location = new System.Drawing.Point(178, 152);
+            this.txtTimKiem.Name = "txtTimKiem";
+            this.txtTimKiem.Size = new System.Drawing.Size(184, 22);
+            this.txtTimKiem.TabIndex = 5;
+            this.txtTimKiem.TextChanged += new System.EventHandler(this.txtTimKiem_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightPink;
+            this.label1.Location = new System.Drawing.Point(57, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nhập thông tin";
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.BackColor = System.Drawing.Color.Tomato;
+            this.btnTimKiem.Location = new System.Drawing.Point(386, 147);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(73, 33);
+            this.btnTimKiem.TabIndex = 7;
+            this.btnTimKiem.Text = "Tìm";
+            this.btnTimKiem.UseVisualStyleBackColor = false;
+            this.btnTimKiem.Click += new System.EventHandler(this.btnTimKiem_Click);
+            // 
+            // btnSapXep
+            // 
+            this.btnSapXep.BackColor = System.Drawing.Color.LightCoral;
+            this.btnSapXep.ForeColor = System.Drawing.Color.Black;
+            this.btnSapXep.Location = new System.Drawing.Point(119, 203);
+            this.btnSapXep.Name = "btnSapXep";
+            this.btnSapXep.Size = new System.Drawing.Size(96, 44);
+            this.btnSapXep.TabIndex = 8;
+            this.btnSapXep.Text = "Sắp xếp";
+            this.btnSapXep.UseVisualStyleBackColor = false;
+            this.btnSapXep.Click += new System.EventHandler(this.btnSapXep_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.BackColor = System.Drawing.Color.LightCoral;
+            this.btnThoat.ForeColor = System.Drawing.Color.Black;
+            this.btnThoat.Location = new System.Drawing.Point(262, 203);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(100, 44);
+            this.btnThoat.TabIndex = 9;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = false;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // listView2
+            // 
+            this.listView2.BackColor = System.Drawing.Color.LightPink;
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(34, 136);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(450, 52);
+            this.listView2.TabIndex = 4;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.SelectedIndexChanged += new System.EventHandler(this.listView2_SelectedIndexChanged);
+            // 
+            // frmTuyChon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(539, 276);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnSapXep);
+            this.Controls.Add(this.btnTimKiem);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTimKiem);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.rdNgaySinh);
+            this.Controls.Add(this.rdHoTen);
+            this.Controls.Add(this.rdMaSV);
+            this.Controls.Add(this.listView1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "frmTuyChon";
+            this.Text = "Tùy Chọn";
+            this.Load += new System.EventHandler(this.frmTuyChon_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.RadioButton rdMaSV;
+        private System.Windows.Forms.RadioButton rdHoTen;
+        private System.Windows.Forms.RadioButton rdNgaySinh;
+        private System.Windows.Forms.TextBox txtTimKiem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnTimKiem;
+        private System.Windows.Forms.Button btnSapXep;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.ListView listView2;
+    }
+}
---
+++ Lab03_Demo/Lab03_Demo/FormTuyChon.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab03_Demo
+{
+    public class SinhVienSortNgaySinh : IComparer<SinhVien>
+    {
+        public int Compare(SinhVien x, SinhVien y)
+        {
+            return x.NgaySinh.CompareTo(y.NgaySinh);
+        }
+    }
+
+    public class SinhVienSortMaSo : IComparer<SinhVien>
+    {
+        public int Compare(SinhVien x, SinhVien y)
+        {
+            return int.Parse(x.MaSo) - int.Parse(y.MaSo);
+        }
+    }
+
+    public class SinhVienSortHoTen : IComparer<SinhVien>
+    {
+        public int Compare(SinhVien x, SinhVien y)
+        {
+            var firstWords = x.HoTen.Split(' ');
+            var secondWords = y.HoTen.Split(' ');
+
+            var firstName = firstWords[firstWords.Length - 1];
+            var secondName = secondWords[secondWords.Length - 1];
+
+            return firstName.CompareTo(secondName);
+        }
+    }
+}
